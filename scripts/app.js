b863d1687a22845e9313ac038213b9f34160d4c8
MartinNevlaha <martin.nevlaha@gmail.com>
2021-01-28 18:37:05 +0100

    styled add category input and added close function to app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,11 @@
 const addCatButton = document.getElementById("backdropOpen");
 const backdrop = document.querySelector(".backdrop");
+const closeBacdrop = document.getElementById("closeBackdrop");
 
 addCatButton.addEventListener("click", () => {
-    backdrop.classList.add("open");
+  backdrop.classList.add("open");
 });
 
-backdrop.addEventListener("click", () => {
-    backdrop.classList.remove("open");
-})
+closeBacdrop.addEventListener("click", () => {
+  backdrop.classList.remove("open");
+});
