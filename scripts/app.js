a0f0e80bb38e310abbd49e03510f3642f68cd5be
MartinNevlaha <martin.nevlaha@gmail.com>
2021-01-28 18:58:11 +0100

    added hovered class

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 const addCatButton = document.getElementById("backdropOpen");
 const backdrop = document.querySelector(".backdrop");
 const closeBacdrop = document.getElementById("closeBackdrop");
+const items = document.querySelectorAll(".gallery_content_item");
 
 addCatButton.addEventListener("click", () => {
   backdrop.classList.add("open");
@@ -9,3 +10,12 @@ addCatButton.addEventListener("click", () => {
 closeBacdrop.addEventListener("click", () => {
   backdrop.classList.remove("open");
 });
+
+items.forEach(item => {
+    item.addEventListener("mouseover", () => {
+        item.classList.add("hovered");
+    })
+    item.addEventListener("mouseout", () => {
+        item.classList.remove("hovered");
+    })
+})
